629eed099c1d47bcd012cf553016c149cfdd8135
othonaf <othon.ab@hotmail.com>
2021-12-12 16:49:03 -0300

    Atualizando o arquivo exercício.js

--- modulo1/revisao-js/lista-revisao-js/exercicios.js
+++ modulo1/revisao-js/lista-revisao-js/exercicios.js
@@ -17,33 +17,48 @@ function retornaArrayInvertido(array) {
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-    arrayOrdenado = array.sort((a, b) => a-b)
+    const arrayOrdenado = array.sort((a, b) => a-b)
     return arrayOrdenado
 }
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+    const arrayDePares = array.filter(pares => (pares %2)== 0)
+    return arrayDePares
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+    const arrayDePares = array.filter(pares => (pares %2)== 0)
+    const arrayDeElevados = arrayDePares.map(pares => {
+        return Math.pow(pares, 2) 
+    })
+        
+    return arrayDeElevados
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+    const maiorDeTodos = array.reduce(function(a, b) {
+        return Math.max(a, b)
+    })
+    return maiorDeTodos
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
-
+     
 }
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+   const tresPrimeirosPares = []
+   for (let i = 0; tresPrimeirosPares.length < n; i++){
+       if(i % 2 === 0){
+           tresPrimeirosPares.push(i)
+       }
+   }
+   return tresPrimeirosPares 
 }
 
 // EXERCÍCIO 09
@@ -53,7 +68,7 @@ function classificaTriangulo(ladoA, ladoB, ladoC) {
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+    const segundos = array.filter(maiorEmenor => )
 }
 
 // EXERCÍCIO 11
