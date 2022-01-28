01c3841ad5fef160f077ea144b441c5a0d749a54
othonaf <othon.ab@hotmail.com>
2022-01-27 22:26:07 -0300

    Enviando os arquivos do Labedin.

---
+++ modulo3/props-labedin/labedin/src/App.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import './App.css';
+import CardGrande from './components/CardGrande/CardGrande';
+import ImagemButton from './components/ImagemButton/ImagemButton';
+import FotoPerfil from "./img/FotoPerfil.PNG";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="page-section-container">
+        <h2>Dados pessoais</h2>
+        <CardGrande 
+          imagem=".img/FotoPerfil.PNG" 
+          nome="Othon Fontenele" 
+          descricao="Oi, eu sou o Othon. Sou programador fullstack, trabalho com javascript, React, gitHub, node e mais um pouco."
+        />
+        
+        <ImagemButton 
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+          texto="Ver mais"
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Experiências profissionais</h2>
+        <CardGrande 
+          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
+          nome="Othon" 
+          descricao="Atualmente cursando programação fullstack pela Labenu." 
+        />
+        
+        <CardGrande 
+          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
+          nome="NASA" 
+          descricao="Apontando defeitos." 
+        />
+      </div>
+
+      <div className="page-section-container">
+        <h2>Minhas redes sociais</h2>
+        <ImagemButton 
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
+          texto="Facebook" 
+        />        
+
+        <ImagemButton 
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
+          texto="Twitter" 
+        />        
+      </div>
+
+      {/* <div className="page-section-container">
+       
+        <CardPequeno 
+          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
+          nome="NASA" 
+          descricao="Apontando defeitos." 
+        />
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
