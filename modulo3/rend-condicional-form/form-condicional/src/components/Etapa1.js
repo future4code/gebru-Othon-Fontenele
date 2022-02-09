1c82e4e4834ed759ce8c8c7652ba0e78d813b36d
othonaf <othon.ab@hotmail.com>
2022-02-08 23:55:41 -0300

    Enviando arquivos Forms Condicionais.

---
+++ modulo3/rend-condicional-form/form-condicional/src/components/Etapa1.js
@@ -0,0 +1,19 @@
+import react from 'react';
+import '../App.css';
+
+function Etapa1() {
+  return (
+    <div className="Etapa1">
+      <h1>Etapa 1 - Dados Gerais</h1>
+      
+      <form onSubmit={}>
+        <label>
+        1 Qual o seu nome?
+        <input type="text" value={} onChange={} />
+        </label>
+      </form>
+    </div>
+  );
+}
+
+export default Etapa1;
