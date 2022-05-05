5823b019adf33c0722c2a0e37b17d959436fc738
othonaf <othon.ab@hotmail.com>
2022-05-05 19:46:11 -0300

    Enviando a parte 5 do projeto StarWars

--- modulo4/starwars/star-wars-app/src/CharacterDetailPage/CharacterDetailPage.jsx
+++ modulo4/starwars/star-wars-app/src/CharacterDetailPage/CharacterDetailPage.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styledComponents from "styled-components";
 
 const CharacterDetailPage = (props) => {
+    const [details, setDatails] = useState({})
+    const [planet, setPlanet] =useState({})
+
+    useEffect(() => {
+        getCharacterDetails()
+    }, [props.url])
+
+    useEffect(() => {
+        getPlanetDetails()
+    }, [details.homeworld])
+
+    const getCharacterDetails = () => {
+        axios.get(props.url)
+        .then((response) => {
+            setDatails(response.data)
+        })
+        .catch((error) => console.log(error.message))
+    }
+
+    const getPlanetDetails = () => {
+        axios.get(details.homeworld)
+        .then((response) => {
+            setPlanet(response.data)
+        })
+        .catch((error) => console.log(error.message))
+    }
     return (
         <div>
             <h1>Detalhes dos Personagens</h1>
-            <p>{props.url}</p>
-            <button onClick={props.goToListPage}>Voltar</button>
+            {details.name && planet.name ? 
+                <div>
+                    <p>Nome: {details.name}</p>
+                    <p>Planeta de Origem: {planet.name}</p>
+                </div> :
+                <p>Carregando...</p>
+        }
+        <button onClick={props.goToListPage}>Voltar</button>
         </div>
     )
 }
